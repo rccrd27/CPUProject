d939ee65fc17ceba88fcfc7378915a6034b56870
ebby-s <ebbysamson@gmail.com>
2020-05-19 23:09:06 +0100

    Stack, Task 1 and 3 tested

--- HarvardArchNoPipeline/Decoder.v
+++ HarvardArchNoPipeline/Decoder.v
@@ -3,11 +3,14 @@ module Decoder
 	input [2:0] state,
 	input [3:0] inst,
 	input eq,
+	output stack_mux,
 	output acc_load,
-	output e,
 	output WrEn,
 	output pc_load,
-	output pc_inc
+	output pc_inc,
+	output e,
+	output push,
+	output pop
 );
 	wire lda, sta, jmp, stp, jms, bbl, ldr, jeq;
 	wire fetch, exec1, exec2;
@@ -18,7 +21,7 @@ module Decoder
 	assign lda = ~inst[3] & inst[2] & ~inst[1] & inst[0];
 	assign jms = ~inst[3] & inst[2] & inst[1] & ~inst[0];
 	assign bbl = ~inst[3] & inst[2] & inst[1] & inst[0];
-	assign ldr = inst[3] & inst[2] & ~inst[1] & inst[0];
+	assign ldr = inst[3] & inst[2] & inst[1] & ~inst[0];
 	assign jeq = ~inst[3] & ~inst[2] & ~inst[1];
 	
 	assign fetch = state[0];
@@ -27,9 +30,12 @@ module Decoder
 	
 	assign e = lda |ldr;
 	assign WrEn = exec1 & sta;
-	assign pc_load = exec1 & (jmp | jeq & ~eq);
-	assign pc_inc = exec1 & ~(stp | (jmp | jeq & ~eq));
+	assign pc_load = exec1 & (jmp | jeq & ~eq | bbl | jms);
+	assign pc_inc = exec1 & ~(stp | (jmp | jeq & ~eq | bbl | jms));
 	assign acc_load = exec2 & (lda |ldr);
+	assign stack_mux = bbl;
+	assign push = jms & exec1;
+	assign pop = bbl & exec1;
 
 endmodule
 
