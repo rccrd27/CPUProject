06d4f7b271baadbfdfd435e5f56b66d924b309d3
ebby-s <31300178+ebby-s@users.noreply.github.com>
2020-05-20 23:09:11 +0100

    Verilog multiplier in arm_alu

--- HarvardArch/Decoder.v
+++ HarvardArch/Decoder.v
@@ -3,38 +3,40 @@ module Decoder
 	input [2:0] state,
 	input [3:0] inst,
 	input eq,
+	output stack_mux,
 	output acc_load,
-	output e,
 	output WrEn,
 	output pc_load,
 	output pc_inc,
-	output p,
-	output prog_mux
-	
+	output e,
+	output push,
+	output pop,
+	output jump_mux
 );
 	wire lda, sta, jmp, stp, jms, bbl, ldr, jeq;
 	wire fetch, exec1, exec2;
 	
-	assign sta = ~inst[3] & ~inst[2] & inst[1] & ~inst[0];
-	assign jmp = ~inst[3] & ~inst[2] & inst[1] & inst[0];
+	assign sta = ~inst[3] & ~inst[2] & ~inst[1] & ~inst[0];
+	assign jmp = ~inst[3] & ~inst[2] & ~inst[1] & inst[0];
 	assign stp = ~inst[3] & inst[2] & ~inst[1] & ~inst[0];
 	assign lda = ~inst[3] & inst[2] & ~inst[1] & inst[0];
 	assign jms = ~inst[3] & inst[2] & inst[1] & ~inst[0];
 	assign bbl = ~inst[3] & inst[2] & inst[1] & inst[0];
-	assign ldr = inst[3] & inst[2] & ~inst[1] & inst[0];
-	assign jeq = ~inst[3] & ~inst[2] & ~inst[1];
+	assign ldr = inst[3] & inst[2] & inst[1] & ~inst[0];
+	assign jeq = ~inst[3] & ~inst[2] & inst[1];
 	
 	assign fetch = state[0];
 	assign exec1 = state[1];
 	assign exec2 = state[2];
 	
-	assign p = 0; //~fetch & (lda | jmp);
 	assign e = lda |ldr;
 	assign WrEn = exec1 & sta;
-	assign pc_load = exec1 & (jmp | jeq & ~eq);
-	assign pc_inc = exec1 & ~(stp | (jmp | jeq & ~eq));
+	assign pc_load = exec1 & (stp | jmp | jeq & ~eq | bbl | jms);
+	assign pc_inc = (fetch | exec2);
 	assign acc_load = exec2 & (lda |ldr);
-	assign prog_mux = 0;
+	assign stack_mux = bbl;
+	assign push = jms & exec1;
+	assign pop = bbl & exec1;
+	assign jump_mux = exec1 & (stp | jmp | jeq & ~eq | bbl | jms);
 
-endmodule
-
+endmodule 
