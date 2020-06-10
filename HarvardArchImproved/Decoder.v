f4c9a6af4637786132f781b21528ffd984f0aaa4
ebby-s <ebbysamson@gmail.com>
2020-06-10 15:13:40 +0100

    HAI turing complete, 32 max depth

--- HarvardArchImproved/Decoder.v
+++ HarvardArchImproved/Decoder.v
@@ -3,7 +3,7 @@ module Decoder
 	input [3:0] state,
 	input [3:0] inst,
 	input eq,
-	output stack_mux,
+	output [1:0] jump_mux,
 	output WrEn,
 	output pc_load,
 	output pc_inc,
@@ -12,9 +12,10 @@ module Decoder
 	output m,
 	output push,
 	output pop,
-	output data_mux
+	output data_mux,
+	output load_mux
 );
-	wire lda, sta, jmp, stp, jms, bbl, ldr, mul, jeq;
+	wire lda, sta, jmp, stp, jms, bbl, ldr, mul, jeq, jmc;
 	wire fetch, exec1, exec2, exec3;
 	
 	assign sta = ~inst[3] & ~inst[2] & ~inst[1] & ~inst[0];
@@ -26,6 +27,7 @@ module Decoder
 	assign ldr = inst[3] & inst[2] & inst[1] & ~inst[0];
 	assign mul = inst[3] & inst[2] & ~inst[1] & inst[0];
 	assign jeq = ~inst[3] & inst[2] & inst[1] & ~inst[0];
+	assign jmc = ~inst[3] & inst[2] & inst[1] & inst[0];
 	
 	assign fetch = state[0];
 	assign exec1 = state[1];
@@ -38,10 +40,12 @@ module Decoder
 	assign pc_load = exec1 & (stp | jmp | jeq & ~eq | bbl | jms);
 	assign pc_inc = fetch | (exec1 & ~e) | (exec2 & ~m) | exec3;
 	assign acc_load = exec2 & (lda | ldr);
-	assign stack_mux = bbl;
+	assign jump_mux[0] = bbl;
+	assign jump_mux[1] = jmc;
 	assign push = jms & exec1;
 	assign pop = bbl & exec1;
 	assign data_mux = ldr;
+	assign load_mux = bbl;
 
 endmodule
 
--- HarvardArchImproved/stack_pointer.v
+++ HarvardArchImproved/stack_pointer.v
@@ -1,14 +1,20 @@
 module stack_pointer
 (
-  input [2:0] s,
+  input [4:0] s,
   input push,
   input pop,
-  output [2:0] n
+  output [4:0] n
 );
 
-  // assign n = s + {2'b00, push} + {2'b11, ~pop} + {3'b001};
+  assign n = s + {4'b0000, push} + {4'b1111, ~pop} + {5'b00001};
+
+  /*
   assign n[0] = s[0] & ~push & ~pop | ~s[0] & push | ~s[0] & pop;
   assign n[1] = s[1] & s[0] & ~push | s[1] & ~s[0] & ~pop | ~s[1] & s[0] & push | ~s[1] & ~s[0] & pop;
-  assign n[2] = ~s[2] & s[1] & s[0] & push | s[2] & ~s[1] & ~s[0] & ~pop | s[2] & s[1] | s[2] & s[0];
+  assign n[2] = s[2] & s[1] & s[0] & ~push | s[2] & s[1] & ~s[0] | s[2] & ~s[1] & s[0] | s[2] & ~s[1] & ~s[0] & ~pop | ~s[2] & s[1] & s[0] & push | ~s[2] & ~s[1] & ~s[0] & pop;
+  assign n[3] = s[3] & s[2] & s[1] & s[0] & ~push | s[3] & s[2] & ~s[1] | s[3] & ~s[2] & s[0] | s[3] & s[1] & ~s[0] | s[3] & ~s[2] & ~s[1] & ~s[0] & ~pop | ~s[3] & s[2] & s[1] & s[0] & push | ~s[3] & ~s[2] & ~s[1] & ~s[0] & pop;
+  assign n[4] = s[4] & s[3] & s[2] & s[1] & s[0] & ~push | s[4] & ~s[3] & s[0] | s[4] & s[1] & ~s[0] | s[4] & s[2] & ~s[1] | s[4] & s[3] & ~s[2] | s[4] & ~s[3] & ~s[2] & ~s[1] & ~s[0] & ~pop |
+                ~s[4] & s[3] & s[2] & s[1] & s[0] & push | ~s[4] & ~s[3] & ~s[2] & ~s[1] & ~s[0] & pop;
+  */
 
 endmodule 
