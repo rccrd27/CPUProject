ba1667be81b491673207843e18d04bc240249922
ebby-s <ebbysamson@gmail.com>
2020-06-11 13:07:03 +0100

    No more changes for real this time

--- HarvardArchImproved/Decoder.v
+++ HarvardArchImproved/Decoder.v
@@ -13,9 +13,10 @@ module Decoder
 	output push,
 	output pop,
 	output data_mux,
-	output load_mux
+	output load_mux,
+	output reg_mux
 );
-	wire lda, sta, jmp, stp, jms, bbl, ldr, mul, jeq, jmc;
+	wire lda, sta, jmp, stp, jms, bbl, ldr, str, mul, jeq, jmc;
 	wire fetch, exec1, exec2, exec3;
 	
 	assign sta = ~inst[3] & ~inst[2] & ~inst[1] & ~inst[0];
@@ -25,6 +26,7 @@ module Decoder
 	assign jms = ~inst[3] & inst[2] & ~inst[1] & ~inst[0];
 	assign bbl = ~inst[3] & inst[2] & ~inst[1] & inst[0];
 	assign ldr = inst[3] & inst[2] & inst[1] & ~inst[0];
+	assign str = inst[3] & inst[2] & inst[1] & inst[0];
 	assign mul = inst[3] & inst[2] & ~inst[1] & inst[0];
 	assign jeq = ~inst[3] & inst[2] & inst[1] & ~inst[0];
 	assign jmc = ~inst[3] & inst[2] & inst[1] & inst[0];
@@ -36,9 +38,9 @@ module Decoder
 	
 	assign e = lda | ldr | mul;
 	assign m = mul;
-	assign WrEn = exec1 & sta;
+	assign WrEn = exec1 & (sta | str);
 	assign pc_load = exec1 & (stp | jmp | jeq & ~eq | bbl | jms);
-	assign pc_inc = fetch | (exec1 & ~e) | (exec2 & ~m) | exec3;
+	assign pc_inc = exec1 & ~stp; // fetch | (exec1 & ~e) | (exec2 & ~m) | exec3;
 	assign acc_load = exec2 & (lda | ldr);
 	assign jump_mux[0] = bbl;
 	assign jump_mux[1] = jmc;
@@ -46,6 +48,7 @@ module Decoder
 	assign pop = bbl & exec1;
 	assign data_mux = ldr;
 	assign load_mux = bbl;
+	assign reg_mux = str;
 
 endmodule
 
